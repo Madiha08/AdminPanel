b986101d45d2afe87f50ac3238e8e8637adb13ad
laiba <152904200+Madiha08@users.noreply.github.com>
2023-12-17 17:59:24 -0800

    file updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadComponent } from './Admin/head/head.component';
+import { FootComponent } from './Admin/foot/foot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    FootComponent
   ],
   imports: [
     BrowserModule,
